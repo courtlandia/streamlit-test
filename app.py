90fbfd46cb148224e56fef7e927d96938eedab3b
courtlandia <99390213+courtlandia@users.noreply.github.com>
2023-04-25 17:12:04 -0700

    Update app.py

--- app.py
+++ app.py
@@ -27,22 +27,8 @@ if uploaded_file is not None:
     x_axis = st.selectbox("Select X Axis", columns)
     y_axis = st.selectbox("Select Y Axis", columns)
 
-    # Allow the user to filter the data
-    filtered_df = df
-    sidebar_selectbox = sidebar.selectbox(
-        "Select an option",
-        ["No filter", "Filter by column"]
-    )
-    if sidebar_selectbox == "Filter by column":
-        filter_column = st.selectbox("Select Filter Column", columns)
-        filter_value = st.text_input("Enter Filter Value")
-        if filter_value:
-            filtered_df = filtered_df[filtered_df[filter_column] == filter_value]
-            if st.button("Clear filter"):
-                filtered_df = df
-
     # Create a stacked bar chart using Plotly
-    fig = px.bar(filtered_df, x=x_axis, y=y_axis, color=y_axis, barmode='stack')
+    fig = px.bar(df, x=x_axis, y=y_axis, color=y_axis, barmode='stack')
     fig.update_layout(
         width=700,
         height=500,
@@ -56,7 +42,7 @@ if uploaded_file is not None:
             'xanchor': 'center'
         },
         xaxis_title=x_axis,
-        yaxis_title=y_axis
+        yaxis_title="Sum"
     )
 
     st.plotly_chart(fig, use_container_width=True)
