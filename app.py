4fccc9f4f66188ef4efe5b1a307b09d6c6035c8e
courtlandia <99390213+courtlandia@users.noreply.github.com>
2023-04-26 13:17:01 -0700

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ sidebar.title("Upload CSV")
 uploaded_file = sidebar.file_uploader("Choose a CSV file", type="csv")
 
 # Main page content
-st.title("Stacked Bar Chart with Plotly")
+st.title("Stacked Bar Chart with Plotly Express")
 st.write("Upload a CSV file and select columns to visualize.")
 
 # Load data if a file has been uploaded
@@ -26,23 +26,10 @@ if uploaded_file is not None:
     columns = list(df.columns)
     x_axis = st.selectbox("Select X Axis", columns)
     y_axis = st.selectbox("Select Y Axis", columns)
+    color_column = st.selectbox("Select Color Column", columns)
 
-    # Create a stacked bar chart using Plotly
-    fig = px.bar(df, x=x_axis, y=y_axis, color=y_axis, barmode='stack')
-    fig.update_layout(
-        width=700,
-        height=500,
-        drillmode="select"
-    )
-    fig.update_traces(texttemplate='%{text:.2s}', textposition='auto')
-    fig.update_layout(
-        title={
-            'text': "Stacked Bar Chart with Plotly",
-            'x': 0.5,
-            'xanchor': 'center'
-        },
-        xaxis_title=x_axis,
-        yaxis_title="Sum"
-    )
+    # Create a stacked bar chart using Plotly Express
+    fig = px.bar(df, x=x_axis, y=y_axis, color=color_column, barmode='stack')
 
+    # Display the chart
     st.plotly_chart(fig, use_container_width=True)
